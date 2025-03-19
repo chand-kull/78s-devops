a34a3f7e45a33273257102914c7cf7d34009dcc6
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-03-19 17:24:17 +0530

    this is my first commit

---
+++ 01shall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "hey hai this is chandu"
+echo "yeah chandu tell me what can i do for you "
---
+++ installlation-shall.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    echo "Exit status:$1"
+    echo "What are you doing: $2"
+}
+
+if [ $userid -ne 0]
+then 
+    echo "please run this script with root access."
+    exitt 1 
+else
+    ccho "you are super user."
+if
+
+dnf install mysql -y
+VALIDATE $? "installing MYSQL"
+
+dnf install git -y 
+VALIDATE $? "installing MYSQL"
