0a2fd737ae040a681755a3ff143846b2fde3b75b
chand-kull <cheerlachandrasekhar81@gmail.com>
2025-03-19 15:26:45 +0530

    this is my first commit

---
+++ shall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "hey hai this is chandu"
+echo "ok fine hai chandu how are you man "
+echo "what can i do for you "
+echo "nothing jest casually i am comming "
+echo "ok fine nice talking to you "
